2d0e0f9a72eeb33a40cc70192e9cda491455312b
Roppest <rodvaudio@gmail.com>
2021-03-19 15:39:26 -0600

    Revert "[UPDATE] Vulnerabilidades pueden ser cambiadas"
    This reverts commit 891665708f3bdc8db4edb7556c76fb3d1af694c9.

--- README.md
+++ README.md
@@ -1,22 +1,7 @@
 # Proyecto en Django para micositio de Semaforo de Riesgos
-### Para Instalar
-```bash
-$ pip install -r requirements.txt
-$ python inicializar_simulacion.py
-```
-#### **Si fontawesome-free no se encuentra**
-```bash
-$ pip install fontawesome-free
-```
-### Para Corrrer
+### Para correr
 
 ```bash
+$ python inicializar_simulacion.py
 $ python manage.py runserver
 ```
-### Rutas Disponibles
-- /simulacion
-
-[http://localhost:8000/simulacion](http://localhost:8000/simulacion)
-- /simulacion/caso
-
-[http://localhost:8000/simulacion/caso](http://localhost:8000/simulacion/caso)
--- semaforoDeRiesgos/static/simulacion/simulacion.js
+++ semaforoDeRiesgos/static/simulacion/simulacion.js
@@ -1,4 +1,3 @@
-
 function getColor(valor){
     if(valor < 5.0)
         return '#00D024'
@@ -53,81 +52,8 @@ function renderMovilidadOptions(container_id){
     }
         
 }
-
-function renderSensorialOptions(container_id){
-   
-    let sensorial={
-        '2.5' : 'No puedo escuchar alarmas ni visualizar rutas de evacuación.',
-        '1.5': 'Se me dificulta ver o escuchar alarmas e indicaciones de evacuación.',
-        '0.5': 'Puedo escuchar indicaciones y alarmas claramente y observar rutas de evacuación.'
-    };
-    let opciones=[]
-    for(key in sensorial){
-        let opt=document.createElement('option');
-        opt.text=sensorial[key];
-        opt.value=key;
-        opt.className='vulnerabilidades';
-        opciones.push(opt);  
-    }
+function recalcularIr(caso){
     
-    opciones.sort((a,b)=>{
-        if (a.value < b.value)
-            return -1;
-        else
-            return 1;
-    });
-    let container=document.getElementById(container_id);
-    for(op in opciones){
-        container.appendChild(opciones[op]);
-    }
-        
-}
-function renderUbiOptions(container_id){
-   
-    let ubicacion={
-        '2.5': 'Del séptimo piso en adelante de un inmueble.',
-        '2': 'Del cuarto al sexto piso de un inmueble.',
-        '1.5': 'En el tercer o segundo piso de un inmueble.',
-        '1': 'En la planta baja de un inmueble.',
-        '0.5': 'En zona despejada.'
-    };
-    let opciones=[]
-    for(key in ubicacion){
-        let opt=document.createElement('option');
-        opt.text=ubicacion[key];
-        opt.value=key;
-        opt.className='vulnerabilidades';
-        opciones.push(opt);  
-    }
-    
-    opciones.sort((a,b)=>{
-        if (a.value < b.value)
-            return -1;
-        else
-            return 1;
-    });
-    let container=document.getElementById(container_id);
-    for(op in opciones){
-        container.appendChild(opciones[op]);
-    }
-        
-}
-function recalcularIR(v){
-    let vulnerabilidad_original = document.getElementById('vulnerabilidad').innerText;
-    let d_fis = parseFloat(document.getElementById('movilidad_select').value);
-    let d_sen = parseFloat(document.getElementById('sensorial_select').value);
-    let u = parseFloat(document.getElementById('ubi_select').value);
-    let vul = DISTANCIA+d_fis+d_sen+u;
-    document.getElementById('vulnerabilidad').innerHTML = vul;
-    let p = parseFloat(document.getElementById('peligro').innerText);
-    let ir= (p+vul)/2;
-    setIR(ir);
-    console.log('IR:',ir);
-    let semaforo=indiceASemaforo(ir);
-    console.log('semaforo:',semaforo);
-    document.getElementById('semaforo_header').innerHTML=semaforo.charAt(0).toUpperCase()+semaforo.slice(1);
-    document.getElementById('semaforo').innerHTML=getSemaforo(semaforo);
-    inicializar();
 }
 function setSelectedOption(select_id,option_value){
     let options=document.getElementById(select_id).options;
@@ -144,9 +70,6 @@ function setSelectedOption(select_id,option_value){
         else
             option.selected=false;
     }
-}
-function setIR(ir){
-    document.getElementById('indice').innerHTML=ir;
 }
 function getSensorial(d_sen){
     ponderacion={
@@ -185,20 +108,9 @@ function getSemaforo(sem){
     };
     let key = String(sem);
     if(ponderacion[sem]=== undefined)
-        console.log('Undefined ERROR: Valor de sem =',sem,'no tiene ponderacion');  
+        console.log('Undefined ERROR: Valor de sem =',sem,'no tiene ponderacion')
     else
         document.getElementById('semaforo').style.backgroundColor=ponderacion[sem];
-    return'';
-}
-function indiceASemaforo(ir){
-    if(ir <= 5)
-        return 'verde';
-    else if(5.1<ir && ir<=6.9){
-        return 'amarillo';
-    }
-    else{
-        return 'rojo';
-    }
 }
 
 function inicializar(){
@@ -208,5 +120,6 @@ function inicializar(){
         i.style.color = getColor(i.innerHTML)
         i.style.backgroundColor = '#eee';
     }
+    let vulnerabilidades = document.getElementsByClassName('vulnerabilidades');
     
 }
